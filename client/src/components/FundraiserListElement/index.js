fe5069eafd49e19803c61ee4cbb390db36bed78c
UnderMan4 <filiptheg@gmail.com>
2022-04-02 22:31:34 +0200

    fundraise list

--- client/src/components/FundraiserListElement/index.js
+++ client/src/components/FundraiserListElement/index.js
@@ -3,18 +3,22 @@ import ProgressBar from "../ProgressBar";
 
 const FundraiserListElement = ({ title, description, amount, goal, image }) => {
     return (
-        <div className="itemWrapper">
-            <img src={image} alt="Fundraise background image" />
-            <div className="item">
-                <h1>{title}</h1>
-                <p>{`${description.substring(0, 120)}${
-                    description.length < 120 ? "" : "..."
-                }`}</p>
-                <ProgressBar
-                    amount={amount}
-                    goal={goal}
-                    className="progressBar"
-                />
+        <div className="card">
+            <div className="item-wrapper">
+                {/* <div className="img-wrapper"> */}
+                <img src={image} alt="Fundraise background image" />
+                {/* </div> */}
+                <div className="item">
+                    <h1>{title}</h1>
+                    <p>{`${description.substring(0, 120)}${
+                        description.length < 120 ? "" : "..."
+                    }`}</p>
+                    <ProgressBar
+                        amount={amount}
+                        goal={goal}
+                        className="progressBar"
+                    />
+                </div>
             </div>
         </div>
     );
